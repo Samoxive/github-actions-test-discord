1ded89965554c59a76617f471f992275b9b03d04
Şamil NART <samil.nart@gmail.com>
2021-07-28 23:55:47 +0300

    test

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import Discord from 'discord.js';
+import fs from 'fs';
 const client = new Discord.Client();
 
 client.on('ready', async () => {
 	console.log(`Logged in as ${client.user.tag}!`);
-    console.log(`$GITHUB_WORKSPACE ${process.env.GITHUB_WORKSPACE}`)
+    console.log(`$GITHUB_WORKSPACE ${process.env.GITHUB_WORKSPACE}`);
+    console.log(`CONTENT OF README.md ${fs.readFileSync(`${process.env.GITHUB_WORKSPACE}/README.md`, 'utf8')}`);
     await client.guilds.cache.get('145457131640848384').channels.cache.get('145457131640848384').send('hello')
     process.exit(0);
 });
